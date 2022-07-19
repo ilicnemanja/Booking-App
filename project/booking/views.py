2c0917a24b363b32b19e94e290d19ef348de2fc7
Nemanja Ilic <ilicnemanja.it@gmail.com>
2022-07-19 06:50:36 +0200

    footer section

--- project/booking/views.py
+++ project/booking/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # from .models.person import Person
+from datetime import date
+
+def current_year():
+    current_year = date.today().year
+    return current_year
+
 
 def index(request):
-    
-    return render(request, 'index.html')
+    year = current_year()
+    return render(request, 'index.html', context= {'year': year})
